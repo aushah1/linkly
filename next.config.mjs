d58129dc93579a52a3774094d69a22de9b5becf4
Aushah Gowhar <waniaushahgowhar@gmail.com>
2025-04-23 19:03:30 +0530

    made changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import { withNetlify } from "@netlify/next";
 
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // if you are not using the image optimization
-  },
 };
 
 export default withNetlify(nextConfig);
